172989931c0dbda45a5a5c48350ddada9393246f
ibpop <blazejpop@wp.pl>
2021-11-17 21:11:11 +0100

    initial for MDCEV model

---
+++ MDCEV/LL_mdcev_MATlike.m
@@ -0,0 +1,26 @@
+function [f,g,h]= LL_mdcev_MATlike(data,EstimOpt,OptimOpt,b0)
+% Function calculates loglikelihood of the MDCEV model.
+
+% save res_LL_mnl_MATlike
+% return
+
+probsfun = @(B) probs_mdcev(data,EstimOpt,B);
+
+if isequal(OptimOpt.GradObj,'on') 
+    logprobs = probsfun(b0);
+    if isequal(OptimOpt.Hessian,'user-supplied') == 1
+        j = -numdiff(probsfun,logprobs,b0,isequal(OptimOpt.FinDiffType,'central'),EstimOpt.BActive);
+        g = sum(j,1)';
+        h = j'*j;
+    else
+        j = -numdiff(probsfun,logprobs,b0,isequal(OptimOpt.FinDiffType,'central'),EstimOpt.BActive);
+        g = sum(j,1)';
+    end
+
+    f = -sum(logprobs);
+else
+    EstimOpt.NumGrad = 1;
+    logprobs = probsfun(b0);
+
+    f = -sum(logprobs);   
+end
