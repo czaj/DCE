4b3d58ec8444d0c752456a3857899af47a404655
Mikołaj Czajkowski <mc@uw.edu.pl>
2024-02-19 16:17:28 +0100

    Update norm_var.m
    Added option to normalize for 0-1 range (vs. normalize for unit standard deviation)

--- Tools-master/norm_var.m
+++ Tools-master/norm_var.m
@@ -1,4 +1,15 @@
-function X = norm_var(INPUT)
+function X = norm_var(INPUT,varargin) % option 1 for 0-1 normalization
 
-X = INPUT - nanmean(INPUT);
-X = X ./ nanstd(X);
+if nargin == 1
+    norm_version = 0;
+else
+    norm_version = varargin{1};
+end
+
+if norm_version == 0 % normalize for mean = 0, s.d. = 1
+    X = INPUT - nanmean(INPUT);
+    X = X ./ nanstd(X);
+elseif norm_version == 1
+    X = INPUT - min(INPUT);
+    X = X ./ max(X);
+end
